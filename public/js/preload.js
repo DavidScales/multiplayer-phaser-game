9e3b03adb1d1d4ce3de919954428faef761e4132
David Scales <davidsscales@gmail.com>
2018-09-06 15:16:29 -0700

    feat: reduce to boilerplate

---
+++ public/js/preload.js
@@ -0,0 +1,5 @@
+function preload() {
+  this.load.image('ship', 'assets/spaceShips_001.png');
+  this.load.image('enemy', 'assets/enemy.png');
+  this.load.image('star', 'assets/star_gold.png');
+}
--- server.js
+++ server.js
@@ -1,19 +1,6 @@
 const express = require('express');
 const app = express();
-// can I refactor out http module?
-const server = require('http').Server(app);
-const io = require('socket.io').listen(server);
-
-// a DB here
-const players = {};
-const star = {
-  x: Math.floor(Math.random() * 700) + 50,
-  y: Math.floor(Math.random() * 500) + 50
-};
-const scores = {
-  blue: 0,
-  red: 0
-};
+const port = 8081;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -21,57 +8,6 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-  console.log('a user connected');
-
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-    // remove this player from our players object
-    delete players[socket.id];
-    // emit a message to all players to remove this player
-    io.emit('disconnect', socket.id);
-  });
-
-  // create a new player and add it to our players object
-  players[socket.id] = {
-    rotation: 0,
-    x: Math.floor(Math.random() * 700) + 50,
-    y: Math.floor(Math.random() * 500) + 50,
-    playerId: socket.id,
-    team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
-  };
-  // send the players object to the new player
-  socket.emit('currentPlayers', players);
-  // update all other players of the new player
-  socket.broadcast.emit('newPlayer', players[socket.id]);
-
-  // send the star object to the new player
-  socket.emit('starLocation', star);
-  // send the current scores
-  socket.emit('scoreUpdate', scores);
-
-  socket.on('starCollected', function () {
-    if (players[socket.id].team === 'red') {
-      scores.red += 10;
-    } else {
-      scores.blue += 10;
-    }
-    star.x = Math.floor(Math.random() * 700) + 50;
-    star.y = Math.floor(Math.random() * 500) + 50;
-    io.emit('starLocation', star);
-    io.emit('scoreUpdate', scores);
-  });
-
-  // when a player moves, update the player data
-  socket.on('playerMovement', function (movementData) {
-    players[socket.id].x = movementData.x;
-    players[socket.id].y = movementData.y;
-    players[socket.id].rotation = movementData.rotation;
-    // emit a message to all players about the player that moved
-    socket.broadcast.emit('playerMoved', players[socket.id]);
-  });
-});
-
-server.listen(8081, function () {
-  console.log(`Listening on ${server.address().port}`);
+app.listen(port, function () {
+  console.log(`Listening on ${port}`);
 });
