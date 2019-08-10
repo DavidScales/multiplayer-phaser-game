9264fec9ef853f06a9c6c571fd45dc8d9f047394
David Scales <davidsscales@gmail.com>
2019-08-09 21:20:44 -0700

    Clean up starter files

--- server.js
+++ server.js
@@ -1,31 +1,25 @@
-// server.js
-// where your node app starts
-
-// init project
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+/** API sample reference for later */
+/************************************************************************
 // init sqlite db
-var fs = require('fs');
-var dbFile = './.data/sqlite.db';
-var exists = fs.existsSync(dbFile);
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(dbFile);
+const fs = require('fs');
+const dbFile = './.data/sqlite.db';
+const exists = fs.existsSync(dbFile);
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(dbFile);
 
 // if ./.data/sqlite.db does not exist, create it, otherwise print records to console
 db.serialize(function(){
   if (!exists) {
     db.run('CREATE TABLE Dreams (dream TEXT)');
     console.log('New table Dreams created!');
-    
+
     // insert default dreams
     db.serialize(function() {
       db.run('INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
@@ -40,22 +34,19 @@ db.serialize(function(){
     });
   }
 });
+*********************************************************************** */
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-// endpoint to get all the dreams in the database
-// currently this is the only endpoint, ie. adding dreams won't update the database
-// read the sqlite3 module docs and try to add your own! https://www.npmjs.com/package/sqlite3
-app.get('/getDreams', function(request, response) {
-  db.all('SELECT * from Dreams', function(err, rows) {
-    response.send(JSON.stringify(rows));
-  });
-});
+/** API sample reference for later */
+// app.get('/getDreams', function(request, response) {
+//   db.all('SELECT * from Dreams', function(err, rows) {
+//     response.send(JSON.stringify(rows));
+//   });
+// });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
