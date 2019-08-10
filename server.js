4fe1a620bd0ab2d17ab608844b43b19237769a61
Glitch (daves-game) <none>
2019-08-10 02:29:14 +0000

    👾🎃 Updated with Glitch

--- server.js
+++ server.js
@@ -1,77 +1,61 @@
-const express = require('express');
-const app = express();
-// can I refactor out http module?
-const server = require('http').Server(app);
-const io = require('socket.io').listen(server);
+// server.js
+// where your node app starts
 
-// a DB here
-const players = {};
-const star = {
-  x: Math.floor(Math.random() * 700) + 50,
-  y: Math.floor(Math.random() * 500) + 50
-};
-const scores = {
-  blue: 0,
-  red: 0
-};
+// init project
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/public'));
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// init sqlite db
+var fs = require('fs');
+var dbFile = './.data/sqlite.db';
+var exists = fs.existsSync(dbFile);
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(dbFile);
+
+// if ./.data/sqlite.db does not exist, create it, otherwise print records to console
+db.serialize(function(){
+  if (!exists) {
+    db.run('CREATE TABLE Dreams (dream TEXT)');
+    console.log('New table Dreams created!');
+    
+    // insert default dreams
+    db.serialize(function() {
+      db.run('INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
+    });
+  }
+  else {
+    console.log('Database "Dreams" ready to go!');
+    db.each('SELECT * from Dreams', function(err, row) {
+      if ( row ) {
+        console.log('record:', row);
+      }
+    });
+  }
 });
 
-io.on('connection', function (socket) {
-  console.log('a user connected');
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
 
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-    // remove this player from our players object
-    delete players[socket.id];
-    // emit a message to all players to remove this player
-    io.emit('disconnect', socket.id);
-  });
-
-  // create a new player and add it to our players object
-  players[socket.id] = {
-    rotation: 0,
-    x: Math.floor(Math.random() * 700) + 50,
-    y: Math.floor(Math.random() * 500) + 50,
-    playerId: socket.id,
-    team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
-  };
-  // send the players object to the new player
-  socket.emit('currentPlayers', players);
-  // update all other players of the new player
-  socket.broadcast.emit('newPlayer', players[socket.id]);
-
-  // send the star object to the new player
-  socket.emit('starLocation', star);
-  // send the current scores
-  socket.emit('scoreUpdate', scores);
-
-  socket.on('starCollected', function () {
-    if (players[socket.id].team === 'red') {
-      scores.red += 10;
-    } else {
-      scores.blue += 10;
-    }
-    star.x = Math.floor(Math.random() * 700) + 50;
-    star.y = Math.floor(Math.random() * 500) + 50;
-    io.emit('starLocation', star);
-    io.emit('scoreUpdate', scores);
-  });
-
-  // when a player moves, update the player data
-  socket.on('playerMovement', function (movementData) {
-    players[socket.id].x = movementData.x;
-    players[socket.id].y = movementData.y;
-    players[socket.id].rotation = movementData.rotation;
-    // emit a message to all players about the player that moved
-    socket.broadcast.emit('playerMoved', players[socket.id]);
+// endpoint to get all the dreams in the database
+// currently this is the only endpoint, ie. adding dreams won't update the database
+// read the sqlite3 module docs and try to add your own! https://www.npmjs.com/package/sqlite3
+app.get('/getDreams', function(request, response) {
+  db.all('SELECT * from Dreams', function(err, rows) {
+    response.send(JSON.stringify(rows));
   });
 });
 
-server.listen(8081, function () {
-  console.log(`Listening on ${server.address().port}`);
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
