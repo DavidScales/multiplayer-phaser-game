0b960d69b60d2f5b1fd1f86e878a827276cca03b
David Scales <davidsscales@gmail.com>
2019-08-19 20:18:52 -0700

    TEMP: created actual DB. TODOs: updated methods to actually use DB, figure out timing deals + promisify everything, TODOs in DB file, parity with raining-chain branch + then finish tutorial section. AND TESTS!

--- server/database.js
+++ server/database.js
@@ -1,9 +1,72 @@
 
+const fs = require('fs');
+const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
+
+// init sqlite db
+const dataFolder = path.join(__dirname, '..', '.data');
+if (!fs.existsSync(dataFolder)) { fs.mkdirSync(dataFolder); }
+const dbFile = path.join(dataFolder, 'sqlite.db');
+const DB_EXISTS = fs.existsSync(dbFile);
+const DATABASE = new sqlite3.Database(dbFile);
+
 class Database {
     constructor() {
-        this.users = {
-            'dave': 'admin'
-        }
+        // TODO: probably learn more about SQL
+        // TODO: learn about all the async/timing going on here with serialize
+        // and different operations
+        Database.db.serialize(() => {
+            if (!DB_EXISTS) {
+
+                const config = {
+                    name: 'account',
+                    columns: [
+                        'username TEXT PRIMARY KEY UNIQUE NOT NULL',
+                        'password TEXT NOT NULL', // TODO: Should be salted hash
+                        'created TEXT DEFAULT CURRENT_TIMESTAMP'
+                    ]
+                };
+                Database.createTable(config);
+
+                const tableName = 'account';
+                const mockUsers = [
+                    { username: 'dave', password: 'cool' },
+                    { username: 'skip', password: 'neat' },
+                ];
+                Database.insertItems(tableName, mockUsers);
+            }
+            else {
+                console.log('Database "account" already exists');
+                Database.getAll('account');
+            }
+        });
+    }
+    static createTable(config) {
+        Database.db.run(`CREATE TABLE ${config.name} (${config.columns.join(', ')});`);
+        console.log(`New table "${config.name}" created`);
+    }
+    // TODO: fancy comments on all these like in other game
+    static insertItems(tableName, items) {
+        // TODO: insertItemS should use batch insert
+        Database.db.serialize(() => {
+            items.forEach(item => {
+                const columns = Object.keys(item);
+                const columnStr = columns.join(', ');
+                const valuesStr = columns.map(key => item[key])
+                                        .map(str => `'${str}'`)
+                                        .join(', ');
+                console.log(`INSERT INTO ${tableName} (${columnStr}) VALUES (${valuesStr});`);
+                Database.db.run(`INSERT INTO ${tableName} (${columnStr}) VALUES (${valuesStr});`);
+            });
+        });
+    }
+    static getAll(tableName) {
+        // TODO: Should be an actual GET, and should return instead of logging
+        Database.db.each(`SELECT * from ${tableName};`, (err, row) => {
+            if ( row ) {
+                console.log('record:', row);
+            }
+        });
     }
     isUserNameTaken(username, cb) {
         setTimeout(() => {
@@ -21,58 +84,9 @@ class Database {
           cb(this.users[username] == password);
         }, 10);
       }
-
 }
-
-// // init sqlite db
-// const fs = require('fs');
-// const path = require('path');
-// const dataFolder = path.join(__dirname, '.data');
-// if (!fs.existsSync(dataFolder)) { fs.mkdirSync(dataFolder); }
-// const dbFile = path.join(dataFolder, 'sqlite.db');
-// const dbExists = fs.existsSync(dbFile);
-// const sqlite3 = require('sqlite3').verbose();
-// const db = new sqlite3.Database(dbFile);
-
-// // TODO: probably learn more about SQL
-// db.serialize(function(){
-//   if (!dbExists) {
-//     let columns = [
-//       'username TEXT PRIMARY KEY UNIQUE NOT NULL',
-//       'password TEXT NOT NULL', // TODO: Should be hash
-//       'created TEXT DEFAULT CURRENT_TIMESTAMP'
-//     ];
-//     db.run(`CREATE TABLE account (${columns.join(', ')});`);
-//     console.log('New table "account" created');
-
-//     const mockUsers = [
-//       { username: 'dave', password: 'cool' },
-//       { username: 'skip', password: 'neat' },
-//     ];
-//     db.serialize(function() {
-//       mockUsers.forEach(user => {
-//         const columns = Object.keys(user);
-//         const columnStr = columns.join(', ');
-//         const valuesStr = columns.map(key => user[key])
-//                                  .map(str => `'${str}'`)
-//                                  .join(', ');
-//         console.log(`INSERT INTO account (${columnStr}) VALUES (${valuesStr});`);
-//         db.run(`INSERT INTO account (${columnStr}) VALUES (${valuesStr});`);
-//       });
-//     });
-//   }
-//   else {
-//     console.log('Database "account" already exists');
-//     db.each('SELECT * from account;', function(err, row) {
-//       if ( row ) {
-//         console.log('record:', row);
-//       }
-//     });
-//   }
-// });
+Database.db = DATABASE; // ;)
 
 module.exports = {
     Database: Database
-    // Player: Player,
-    // Bullet: Bullet
 };
